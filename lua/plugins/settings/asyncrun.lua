338e64fe443b1da3230cdd076ec1760a610daa9e
Shatur95 <genaloner@gmail.com>
2021-03-03 00:39:02 +0200

    Fix fugitive integration

--- lua/plugins/settings/asyncrun.lua
+++ lua/plugins/settings/asyncrun.lua
@@ -1,5 +1,7 @@
--- Fugitive integration
-vim.cmd('command! -bang -nargs=* -complete=file Make AsyncRun<bang> -auto=make -program=make -strip <args>')
+-- Git integration
+vim.cmd('command! -bang -bar -nargs=* Gpush execute "AsyncRun<bang> git push" <q-args>')
+vim.cmd('command! -bang -bar -nargs=* Gfetch execute "AsyncRun<bang> git fetch" <q-args>')
+vim.cmd('command! -bang -bar -nargs=* Gpull execute "AsyncRun<bang> git pull" <q-args>')
 
 vim.g.asyncrun_open = 10
 vim.g.asyncrun_rootmarks = {'.git', '.compile_commands.json'}
@@ -8,5 +10,6 @@ vim.api.nvim_set_keymap('', '<C-t>', '<Cmd>call asyncrun#quickfix_toggle(10)<CR>
 vim.api.nvim_set_keymap('i', '<C-t>', '<Esc><Cmd>call asyncrun#quickfix_toggle(10)<CR>', { noremap = true })
 vim.api.nvim_set_keymap('', '<C-BS>', '<Cmd>AsyncStop<CR>', { noremap = true })
 vim.api.nvim_set_keymap('i', '<C-BS>', '<Esc><Cmd>AsyncStop<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<A-y>', '<Cmd>call asyncrun#run("", {}, "git pull --rebase")<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<A-y>', '<Cmd>Gpull<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<A-u>', '<Cmd>Gpush<CR>', { noremap = true })
 vim.api.nvim_set_keymap('', '<Leader>rcd', '<Cmd>execute "cd " . asyncrun#get_root("%")<CR>', { noremap = true })
--- lua/plugins/settings/fugitive.lua
+++ lua/plugins/settings/fugitive.lua
@@ -1,4 +1,3 @@
 vim.api.nvim_set_keymap('n', '<A-g>', '<Cmd>G<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<Leader>gD', '<Cmd>G difftool -y<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<Leader>gd', '<Cmd>SwitchToNormalBuffer<CR><Cmd>Gdiffsplit<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<A-u>', '<Cmd>G push<CR>', { noremap = true })
