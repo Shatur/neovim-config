3f1d1aeb93a8eb23dabfecd4fb9f8bccff6cba45
Shatur95 <genaloner@gmail.com>
2021-03-27 01:05:45 +0200

    Disable list by default

--- init.lua
+++ init.lua
@@ -51,7 +51,6 @@ vim.wo.number = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.cmd('set expandtab') -- https://github.com/neovim/neovim/issues/12978
-vim.wo.list = true
 vim.o.listchars = 'space:⋅,tab:→ ' -- Display tabs
 vim.o.fillchars = 'eob: ' -- Hide ~
 vim.o.whichwrap = vim.o.whichwrap .. 'h,l,<,>,[,]'  -- Wrap movement between lines in edit mode
