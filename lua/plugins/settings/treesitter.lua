d709b6facace5f96c2faf6cf16636a77c1f35882
Shatur95 <genaloner@gmail.com>
2021-04-06 19:06:53 +0300

    Disable tsx language in treesitter Causes issues on Windows.

--- lua/plugins/settings/treesitter.lua
+++ lua/plugins/settings/treesitter.lua
@@ -1,5 +1,6 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = "maintained",
+  ensure_installed = 'maintained',
+  ignore_install = { 'tsx' }, -- Has problems extracting an archive on Windows
   highlight = {
     enable = true
   },
