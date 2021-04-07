6e0f0f8b93476280c37a525b5df5accef0267ba4
Shatur95 <genaloner@gmail.com>
2021-04-07 20:49:41 +0300

    Automatically generate lualine colorscheme.

--- lua/plugins/settings/lualine.lua
+++ lua/plugins/settings/lualine.lua
@@ -1,7 +1,14 @@
-local lualine = require('lualine')
+local auto = require('lualine.themes.auto')
+-- Fix weird bg color in automatically generated theme
+auto.normal.c.bg = auto.normal.b.bg
+auto.insert.c.bg = auto.insert.b.bg
+auto.replace.c.bg = auto.replace.b.bg
+auto.visual.c.bg = auto.visual.b.bg
+auto.command.c.bg = auto.command.b.bg
+
 require('lualine').setup{
   options = {
-    theme = 'palenight',
+    theme = auto,
     section_separators = {},
     component_separators = {'|', '|'},
   },
