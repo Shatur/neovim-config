55d1c061d6137a2436c6521cf0ca4e0a70e9e7de
Shatur95 <genaloner@gmail.com>
2021-04-11 01:04:03 +0300

    Edit lualine colors

--- lua/plugins/settings/lualine.lua
+++ lua/plugins/settings/lualine.lua
@@ -1,15 +1,10 @@
 local zephyr_status = require('lualine.themes.auto')
 local zephyr = require('zephyr')
-zephyr_status.normal.b.bg = zephyr.base2
-zephyr_status.insert.b.bg = zephyr.base2
-zephyr_status.replace.b.bg = zephyr.base2
-zephyr_status.visual.b.bg = zephyr.base2
-zephyr_status.command.b.bg = zephyr.base2
-zephyr_status.normal.c.bg = zephyr.bg2
-zephyr_status.insert.c.bg = zephyr.bg2
-zephyr_status.replace.c.bg = zephyr.bg2
-zephyr_status.visual.c.bg = zephyr.bg2
-zephyr_status.command.c.bg = zephyr.bg2
+zephyr_status.normal.c.bg = zephyr.base2
+zephyr_status.insert.c.bg = zephyr.base2
+zephyr_status.replace.c.bg = zephyr.base2
+zephyr_status.visual.c.bg = zephyr.base2
+zephyr_status.command.c.bg = zephyr.base2
 
 require('lualine').setup{
   options = {
