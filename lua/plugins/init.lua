1e70cf8d467ae427869f68b10abeaa11bc2cdc3e
Shatur95 <genaloner@gmail.com>
2021-04-15 23:37:07 +0300

    Stop using inside VSCode

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,35 +1,15 @@
-require('plugins.settings.easymotion')
-require('plugins.settings.xkbswitch')
-require('plugins.settings.swap')
-require('plugins.settings.subversive')
-
-if vim.fn.exists('g:vscode') == 1 then
-  vim.cmd('packadd! vscode-easymotion')
-  require('plugins.settings.vscode')
-  return
-end
-
-vim.cmd('packadd! asyncrun.vim')
-vim.cmd('packadd! kommentary')
-vim.cmd('packadd! nvim-autopairs')
-vim.cmd('packadd! nvim-cursorline')
-vim.cmd('packadd! open-browser-github.vim')
-vim.cmd('packadd! open-browser.vim')
-vim.cmd('packadd! scroll.vim')
-vim.cmd('packadd! vim-cool')
-vim.cmd('packadd! vim-easymotion')
-vim.cmd('packadd! vim-visual-multi')
-vim.cmd('packadd! vim-visual-star-search')
-vim.cmd('packadd! zephyr-nvim')
-
 require('zephyr') -- Colorscheme
 
 require('plugins.settings.asyncrun')
 require('plugins.settings.autopairs')
 require('plugins.settings.cursorline')
+require('plugins.settings.easymotion')
 require('plugins.settings.kommentary')
 require('plugins.settings.open-browser')
+require('plugins.settings.subversive')
+require('plugins.settings.swap')
 require('plugins.settings.visual-multi')
+require('plugins.settings.xkbswitch')
 
 if vim.fn.exists('g:started_by_firenvim') == 1 then
   vim.cmd('packadd! firenvim')
