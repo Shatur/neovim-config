dd84ad26a3c272aca0b0fbf614a12509817a20ea
Shatur95 <genaloner@gmail.com>
2021-04-19 01:55:01 +0300

    Integrate multiply cursors with statusline

--- lua/plugins/settings/lualine.lua
+++ lua/plugins/settings/lualine.lua
@@ -13,7 +13,8 @@ require('lualine').setup{
     component_separators = {'|', '|'},
   },
   sections = {
+    lualine_a = {require('utils.statusline').mode},
     lualine_c = {'filename', {'diagnostics', sources = {'nvim_lsp'}, color_error = zephyr.red, color_warn = zephyr.orange, color_info = zephyr.fg}},
-    lualine_x = {require('dap').status, require('lsp-status').status, 'encoding', 'fileformat', 'filetype'}
+    lualine_x = {require('dap').status, require('lsp-status').status, require('utils.statusline').cursors, 'encoding', 'fileformat', 'filetype'}
   }
 }
