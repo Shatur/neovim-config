5ba70820076494c10d4525c7c6181a0e3cfe0502
Shatur95 <genaloner@gmail.com>
2021-04-19 01:43:52 +0300

    Update plugins Update cmake.lua, LuaSnip, neovim-cmake, neovim-session-manager, nvim-dap, nvim-lspconfig, nvim-tree.lua, nvim-treesitter, plenary.nvim, telescope.nvim, nvim-autopairs and zephyr-nvim.

--- lua/plugins/settings/cmake.lua
+++ lua/plugins/settings/cmake.lua
@@ -1,3 +1,8 @@
+vim.g.cmake_configure_arguments = '-G Ninja'
+if vim.fn.has('win32') == 1 then
+  vim.g.cmake_configure_arguments = vim.g.cmake_configure_arguments .. ' -D CMAKE_TOOLCHAIN_FILE=C:/ProgramData/vcpkg/scripts/buildsystems/vcpkg.cmake -D VCPKG_TARGET_TRIPLET=x64-windows'
+end
+
 vim.api.nvim_set_keymap('n', '<F5>', '<Cmd>CMake build_and_debug<CR>', { noremap = true })
 vim.api.nvim_set_keymap('i', '<F5>', '<Cmd>CMake build_and_debug<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<S-F5>', '<Cmd>CMake set_target_arguments<CR>', { noremap = true })
