caf1665a57632d62e57fdd691f9e449b3b85533d
Shatur95 <genaloner@gmail.com>
2021-04-18 19:37:42 +0300

    Remove the disable tsx, the issue has been fixed

--- lua/plugins/settings/treesitter.lua
+++ lua/plugins/settings/treesitter.lua
@@ -1,6 +1,5 @@
 require('nvim-treesitter.configs').setup {
   ensure_installed = 'maintained',
-  ignore_install = { 'tsx' }, -- Has problems extracting an archive on Windows
   highlight = {
     enable = true
   },
