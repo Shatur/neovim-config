183dd6ce5155f7a6f65342105a85ca03f17aa619
Shatur95 <genaloner@gmail.com>
2021-04-20 18:23:42 +0300

    Use Powershell on Windows only for integrated terminal

--- lua/plugins/settings/terminal-help.lua
+++ lua/plugins/settings/terminal-help.lua
@@ -2,6 +2,7 @@ vim.g.terminal_default_mapping = false
 vim.g.terminal_list = false
 vim.g.terminal_height = 15
 vim.g.terminal_fixheight = true
+vim.g.terminal_shell = 'powershell'
 
 vim.api.nvim_set_keymap('', '<F4>', '<Cmd>call TerminalToggle()<CR>', { noremap = true })
 vim.api.nvim_set_keymap('i', '<F4>', '<Esc><Cmd>call TerminalToggle()<CR>', { noremap = true })
