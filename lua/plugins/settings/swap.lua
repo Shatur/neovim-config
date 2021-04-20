eb4c510fc67f2f4999f2fe8481392082080afa38
Shatur95 <genaloner@gmail.com>
2021-04-20 22:17:27 +0300

    Fix typo in vim-swap shortcuts

--- lua/plugins/settings/swap.lua
+++ lua/plugins/settings/swap.lua
@@ -2,5 +2,5 @@ vim.g.swap_no_default_key_mappings = true
 
 vim.api.nvim_set_keymap('n', '<Leader>i', '<Plug>(swap-interactive)', {})
 vim.api.nvim_set_keymap('x', '<Leader>i', '<Plug>(swap-interactive)', {})
-vim.api.nvim_set_keymap('n', '<Leader>i', '<Plug>(swap-prev)', {})
-vim.api.nvim_set_keymap('n', '<Leader>i', '<Plug>(swap-next)', {})
+vim.api.nvim_set_keymap('n', '<Leader><Left>', '<Plug>(swap-prev)', {})
+vim.api.nvim_set_keymap('n', '<Leader><Right>', '<Plug>(swap-next)', {})
