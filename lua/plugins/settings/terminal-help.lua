ccaa456bbbc043d020b188230b320556b0c70e24
Shatur95 <genaloner@gmail.com>
2021-04-20 23:15:39 +0300

    Set powershell only on Windows

--- lua/plugins/settings/terminal-help.lua
+++ lua/plugins/settings/terminal-help.lua
@@ -2,7 +2,10 @@ vim.g.terminal_default_mapping = false
 vim.g.terminal_list = false
 vim.g.terminal_height = 15
 vim.g.terminal_fixheight = true
-vim.g.terminal_shell = 'powershell'
+
+if vim.fn.has('win32') == 1 then
+  vim.g.terminal_shell = 'powershell'
+end
 
 vim.api.nvim_set_keymap('', '<F4>', '<Cmd>call TerminalToggle()<CR>', { noremap = true })
 vim.api.nvim_set_keymap('i', '<F4>', '<Esc><Cmd>call TerminalToggle()<CR>', { noremap = true })
