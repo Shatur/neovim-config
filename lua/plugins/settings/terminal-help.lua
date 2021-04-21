8b1e8a323d0d70f05db6bf7c5a6c44c23a119a47
Shatur95 <genaloner@gmail.com>
2021-04-21 21:36:34 +0300

    Use project root for terminal-help

--- lua/plugins/settings/terminal-help.lua
+++ lua/plugins/settings/terminal-help.lua
@@ -2,6 +2,7 @@ vim.g.terminal_default_mapping = false
 vim.g.terminal_list = false
 vim.g.terminal_height = 15
 vim.g.terminal_fixheight = true
+vim.g.terminal_cwd = 2
 
 if vim.fn.has('win32') == 1 then
   vim.g.terminal_shell = 'powershell'
