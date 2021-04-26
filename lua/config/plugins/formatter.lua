f2b1b89a63ecc513dc53ac607a232ca8a8701c28
Shatur95 <genaloner@gmail.com>
2021-04-26 22:37:00 +0300

    Fix formatters configuration

--- lua/config/plugins/formatter.lua
+++ lua/config/plugins/formatter.lua
@@ -2,20 +2,27 @@ require('formatter').setup({
   filetype = {
     lua = {
       function()
-        return {exe = 'lua-format'}
+        return {
+          exe = 'lua-format',
+          stdin = true
+        }
       end
     },
     markdown = {
       function()
         return {
           exe = 'prettier',
-          args = {'--stdin-filepath', vim.api.nvim_buf_get_name(0)}
+          args = {'--stdin-filepath', vim.api.nvim_buf_get_name(0)},
+          stdin = true
         }
       end
     },
     cpp = {
       function()
-        return {exe = 'clang-format'}
+        return {
+          exe = 'clang-format',
+          stdin = true
+        }
       end
     }
   }
