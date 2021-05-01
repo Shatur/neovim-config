4f8fd61e0c413b49e1c7609419c0b27d2f45e309
Shatur95 <genaloner@gmail.com>
2021-05-01 18:23:57 +0300

    Set cwd for update commands

--- lua/config/utils/updater.lua
+++ lua/config/utils/updater.lua
@@ -2,11 +2,11 @@ local updater = {}
 
 function updater.update_plugins(path)
   path = path or 'pack'
-  vim.fn['asyncrun#run']('', {mode = 'term', pos = 'thelp'}, 'git submodule update --remote --jobs=8 ' .. path .. ' && git add --update ' .. path .. ' && git commit -m "Update plugins"')
+  vim.fn['asyncrun#run']('', {mode = 'term', cwd = vim.fn.stdpath('config'), pos = 'thelp'}, 'git submodule update --remote --jobs=8 ' .. path .. ' && git add --update ' .. path .. ' && git commit -m "Update plugins"')
 end
 
 function updater.update_config()
-  vim.fn['asyncrun#run']('', {mode = 'term', pos = 'thelp'}, 'git pull --recurse-submodules --jobs=8')
+  vim.fn['asyncrun#run']('', {mode = 'term', cwd = vim.fn.stdpath('config'), pos = 'thelp'}, 'git pull --recurse-submodules --jobs=8')
 end
 
 return updater
