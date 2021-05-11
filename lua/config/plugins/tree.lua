315f43045fcbe4bcc659f70679d4ecf823503553
Shatur95 <genaloner@gmail.com>
2021-05-11 22:56:12 +0300

    Disable git highlighting in nvim-tree Causes slowdown.

--- lua/config/plugins/tree.lua
+++ lua/config/plugins/tree.lua
@@ -3,32 +3,17 @@ vim.api.nvim_set_keymap('', '<A-f>', '<Cmd>NvimTreeFindFile<CR>', { noremap = tr
 vim.g.nvim_tree_follow = 1
 vim.g.nvim_tree_special_files = {}
 vim.g.nvim_tree_group_empty = 1
-vim.g.nvim_tree_git_hl = 1
+vim.g.nvim_tree_show_icons = {
+   git = 0, -- Causes slowdown
+   folders = 1,
+   files = 1,
+}
 
 vim.g.nvim_tree_icons = {
   default = '',
-    git = {
-      unstaged = '',
-      staged = '',
-      unmerged = '',
-      renamed = '',
-      untracked = '',
-      deleted = ''
-    }
 }
 
 vim.g.nvim_tree_bindings = {
   ['l'] = '<Cmd>lua require"nvim-tree".on_keypress("edit")<CR>',
   ['h'] = '<Cmd>lua require"nvim-tree".on_keypress("close_node")<CR>',
 }
-
--- Setup git highlights
-vim.cmd('highlight! link NvimTreeGitDirty Function')
-vim.cmd('highlight! link NvimTreeGitStaged diffAdded')
-vim.cmd('highlight! link NvimTreeGitMerge diffIndexLine')
-vim.cmd('highlight! link NvimTreeGitRenamed diffChanged')
-vim.cmd('highlight! link NvimTreeGitNew diffNewFile')
-vim.cmd('highlight! link NvimTreeGitDeleted diffRemoved')
-
--- Disable executable decoration
-vim.cmd('highlight! default NvimTreeExecFile guifg=none guibg=none gui=NONE')
