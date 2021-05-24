c74ed427260713eedc305aa3ac45154bdffe4752
Shatur95 <genaloner@gmail.com>
2021-05-24 18:51:00 +0300

    Fix formatting

--- lua/config/utils/buffers.lua
+++ lua/config/utils/buffers.lua
@@ -2,23 +2,23 @@ local buffers = {}
 
 local function is_special_buffer()
   if not vim.api.nvim_buf_get_option(0, 'modifiable') or
-    #vim.api.nvim_buf_get_option(0, 'buftype') ~= 0 or
-    vim.api.nvim_buf_get_option(0, 'buflisted') == false then
+      #vim.api.nvim_buf_get_option(0, 'buftype') ~= 0 or
+      vim.api.nvim_buf_get_option(0, 'buflisted') == false then
     return true
   end
   local filetype = vim.api.nvim_buf_get_option(0, 'filetype')
-  return filetype  == 'gitcommit' or filetype == 'gitrebase'
+  return filetype == 'gitcommit' or filetype == 'gitrebase'
 end
 
 function buffers.switch_to_normal_buffer()
   local current_buffer = vim.api.nvim_get_current_buf()
   while is_special_buffer() do
-      vim.cmd('wincmd w')
-      if current_buffer == vim.api.nvim_get_current_buf() then
-        -- To avoid infinity loop
-        vim.cmd('only')
-        break
-      end
+    vim.cmd('wincmd w')
+    if current_buffer == vim.api.nvim_get_current_buf() then
+      -- To avoid infinity loop
+      vim.cmd('only')
+      break
+    end
   end
 end
 
@@ -32,7 +32,7 @@ function buffers.close_current_buffer()
     return
   end
   local current_buffer = vim.fn.bufnr()
-  if #vim.fn.getbufinfo({buflisted=1}) == 1 then
+  if #vim.fn.getbufinfo({buflisted = 1}) == 1 then
     vim.cmd('enew')
     vim.bo.bufhidden = 'wipe'
   else
