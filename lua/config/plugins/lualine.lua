8bc1aec89d7749591db0a12c7c750d93685a7366
Shatur95 <genaloner@gmail.com>
2021-05-29 11:04:36 +0300

    Specify theme

--- lua/config/plugins/lualine.lua
+++ lua/config/plugins/lualine.lua
@@ -7,11 +7,7 @@ local config = {
     -- Disable sections and component separators
     component_separators = '',
     section_separators = '',
-    theme = {
-      -- We are going to use lualine_c an lualine_x as left and right section. Both are highlighted by c theme.
-      normal = {c = {fg = colors.fg, bg = colors.panel_border}},
-      inactive = {c = {fg = colors.fg, bg = colors.panel_border}}
-    }
+    theme = 'ayu'
   },
   sections = {
     -- Remove the defaults
