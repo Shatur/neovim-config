5d0b7add7c3ccc4aa7f4d456637a2b2a752c1b01
Hennadii Chernyshchyk <genaloner@gmail.com>
2021-06-02 23:05:08 +0300

    Add switch to normal buffer for terminal

--- lua/config/plugins/terminal-help.lua
+++ lua/config/plugins/terminal-help.lua
@@ -8,6 +8,6 @@ if vim.fn.has('win32') == 1 then
   vim.g.terminal_shell = 'powershell'
 end
 
-vim.api.nvim_set_keymap('', '<F4>', '<Cmd>call TerminalToggle()<CR>', { noremap = true })
-vim.api.nvim_set_keymap('i', '<F4>', '<Esc><Cmd>call TerminalToggle()<CR>', { noremap = true })
+vim.api.nvim_set_keymap('', '<F4>', '<Cmd>SwitchToNormalBuffer<CR><Cmd>call TerminalToggle()<CR>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<F4>', '<Cmd>SwitchToNormalBuffer<CR><Esc><Cmd>call TerminalToggle()<CR>', { noremap = true })
 vim.api.nvim_set_keymap('t', '<F4>', '<Cmd>call TerminalToggle()<CR>', { noremap = true })
