075261df3f657bf36d8fec3a03b23c81e2f39fd2
Hennadii Chernyshchyk <genaloner@gmail.com>
2021-07-03 18:22:10 +0300

    Add a snippet for UE_LOG

--- plugin/luasnip.lua
+++ plugin/luasnip.lua
@@ -2,8 +2,20 @@ if vim.g.started_by_firenvim == true then
   return
 end
 
-require'luasnip'.snippets={
-  all = {}
+local luasnip = require('luasnip')
+local snippet = luasnip.snippet
+local snippet_node = luasnip.snippet_node
+local insert_node = luasnip.insert_node
+local text_node = luasnip.text_node
+
+luasnip.snippets = {
+  cpp = {
+    snippet({trig = "log", name = "Log message", dscr = "Print information"}, snippet_node(1, {
+      text_node("UE_LOG(LogTemp, Warning, TEXT("),
+      insert_node(1, "Some warning message"),
+      text_node("));"),
+    }))
+  }
 }
 
 vim.api.nvim_set_keymap('i', '<Tab>', [[luaeval('require("config_utils.completion").tab_snippet()')]], { expr = true })
