6fce65875500fe3c57a88c88b6172049c333d239
Hennadii Chernyshchyk <genaloner@gmail.com>
2021-07-05 23:43:20 +0300

    Add nvim-ts-rainbow

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -9,5 +9,10 @@ require('nvim-treesitter.configs').setup {
   },
   playground = {
     enable = true
+  },
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = 1000
   }
 }
