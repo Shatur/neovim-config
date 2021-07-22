59acbeddeeaf29eafc7338e6fb093d75fb06d458
Hennadii Chernyshchyk <genaloner@gmail.com>
2021-07-22 22:34:47 +0300

    Remove persist_size
    No longer necessary after
    https://github.com/akinsho/nvim-toggleterm.lua/commit/895f8e2485267640ccd11fe3fbf1ab5fe178bc4c

--- plugin/toggleterm.lua
+++ plugin/toggleterm.lua
@@ -6,5 +6,4 @@ require('toggleterm').setup({
   shell = vim.fn.has('win32') == 1 and 'powershell' or '/usr/bin/fish',
   open_mapping = '<F4>',
   shade_terminals = false,
-  persist_size = false,
 })
