2987500212ab823291111597308e4440d1d2b471
Hennadii Chernyshchyk <genaloner@gmail.com>
2021-07-29 00:30:14 +0300

    Use suggested configuration for clang-format

--- plugin/formatter.lua
+++ plugin/formatter.lua
@@ -22,7 +22,9 @@ require('formatter').setup({
       function()
         return {
           exe = 'clang-format',
+          args = {"--assume-filename", vim.api.nvim_buf_get_name(0)},
           stdin = true,
+          cwd = vim.fn.expand('%:p:h'),  -- Run clang-format in cwd of the file.
         }
       end,
     },
