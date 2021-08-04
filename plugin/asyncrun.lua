bb9f16f2a81ec7bdfe668e20b763d197aa9fe1c8
Shatur95 <genaloner@gmail.com>
2021-08-04 18:03:05 +0300

    Configure MSVC environment on Windows

--- plugin/asyncrun.lua
+++ plugin/asyncrun.lua
@@ -1,6 +1,12 @@
 vim.g.asyncrun_open = 10
 vim.g.asyncrun_rootmarks = { '.git', '.compile_commands.json' }
 
+if vim.fn.has('win32') == 1 then
+  -- Add MSVC environment modifier
+  vim.g.asyncrun_program = vim.empty_dict()
+  vim.cmd("let g:asyncrun_program.vcvars64 = { opts -> '\"C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Auxiliary/Build/vcvars64.bat\" && ' .. opts.cmd }")
+end
+
 vim.api.nvim_set_keymap('', '<F3>', '<Cmd>call asyncrun#quickfix_toggle(10)<CR>', { noremap = true })
 vim.api.nvim_set_keymap('i', '<F3>', '<Esc><Cmd>call asyncrun#quickfix_toggle(10)<CR>', { noremap = true })
 vim.api.nvim_set_keymap('', '<C-BS>', '<Cmd>AsyncStop<CR>', { noremap = true })
