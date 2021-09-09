6c69dc60b6da43b8c37e0877abfb5fcb0ca4d899
Hennadii Chernyshchyk <genaloner@gmail.com>
2021-09-09 19:49:42 +0300

    Configure Telescope themes

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -20,6 +20,22 @@ telescope.setup({
     },
   },
   pickers = {
+    spell_suggest = {
+      theme = 'dropdown',
+    },
+    buffers = {
+      theme = 'dropdown',
+    },
+    oldfiles = {
+      theme = 'dropdown',
+    },
+    git_branches = {
+      theme = 'dropdown',
+    },
+    find_files = {
+      theme = 'dropdown',
+      hidden = true,
+    },
     git_commits = {
       mappings = {
         i = {
@@ -39,29 +55,31 @@ telescope.load_extension('asynctasks')
 telescope.load_extension('cmake')
 telescope.load_extension('sessions')
 
-vim.api.nvim_set_keymap('n', 'z=', '<Cmd>Telescope spell_suggest theme=get_dropdown<CR>', { noremap = true })
-vim.api.nvim_set_keymap('', '<S-CR>', '<Cmd>Telescope commands theme=get_dropdown<CR>', { noremap = true })
-vim.api.nvim_set_keymap('i', '<S-CR>', '<Cmd>Telescope commands theme=get_dropdown<CR>', { noremap = true })
-vim.api.nvim_set_keymap('', '<C-Tab>', '<Cmd>Telescope buffers theme=get_dropdown<CR>', { noremap = true })
-vim.api.nvim_set_keymap('i', '<C-Tab>', '<Cmd>Telescope buffers theme=get_dropdown<CR>', { noremap = true })
-vim.api.nvim_set_keymap('', '<F1>', '<Cmd>Telescope help_tags theme=get_dropdown<CR>', { noremap = true })
-vim.api.nvim_set_keymap('i', '<F1>', '<Cmd>Telescope help_tags theme=get_dropdown<CR>', { noremap = true })
-vim.api.nvim_set_keymap('', '<A-o>', '<Cmd>Telescope oldfiles theme=get_dropdown<CR>', { noremap = true })
-vim.api.nvim_set_keymap('i', '<A-o>', '<Cmd>Telescope oldfiles theme=get_dropdown<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<A-b>', '<Cmd>Telescope git_branches theme=get_dropdown<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<A-p>', '<Cmd>SaveSession<CR><Cmd>Telescope sessions theme=get_dropdown<CR>', { noremap = true })
-vim.api.nvim_set_keymap('', '<C-p>', '<Cmd>Telescope find_files hidden=true theme=get_dropdown<CR>', { noremap = true })
-vim.api.nvim_set_keymap('i', '<C-p>', '<Cmd>Telescope find_files hidden=true theme=get_dropdown<CR>', { noremap = true })
-vim.api.nvim_set_keymap('i', '<A-t>', '<Cmd>Telescope asynctasks all theme=get_dropdown<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', 'z=', '<Cmd>Telescope spell_suggest<CR>', { noremap = true })
+vim.api.nvim_set_keymap('', '<S-CR>', '<Cmd>Telescope commands<CR>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<S-CR>', '<Cmd>Telescope commands<CR>', { noremap = true })
+vim.api.nvim_set_keymap('', '<C-Tab>', '<Cmd>Telescope buffers<CR>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-Tab>', '<Cmd>Telescope buffers<CR>', { noremap = true })
+vim.api.nvim_set_keymap('', '<F1>', '<Cmd>Telescope help_tags<CR>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<F1>', '<Cmd>Telescope help_tags<CR>', { noremap = true })
+vim.api.nvim_set_keymap('', '<A-o>', '<Cmd>Telescope oldfiles<CR>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<A-o>', '<Cmd>Telescope oldfiles<CR>', { noremap = true })
+vim.api.nvim_set_keymap('', '<A-b>', '<Cmd>Telescope git_branches<CR>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<A-b>', '<Cmd>Telescope git_branches<CR>', { noremap = true })
+vim.api.nvim_set_keymap('', '<C-p>', '<Cmd>Telescope find_files<CR>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-p>', '<Cmd>Telescope find_files<CR>', { noremap = true })
+vim.api.nvim_set_keymap('', '<A-c>', '<Cmd>Telescope git_commits<CR>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<A-c>', '<Cmd>Telescope git_commits<CR>', { noremap = true })
+vim.api.nvim_set_keymap('', '<A-s>', '<Cmd>Telescope git_stash<CR>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<A-s>', '<Cmd>Telescope git_stash<CR>', { noremap = true })
 vim.api.nvim_set_keymap('', '<A-t>', '<Cmd>Telescope asynctasks all theme=get_dropdown<CR>', { noremap = true })
-vim.api.nvim_set_keymap('i', '<A-c>', '<Cmd>Telescope git_commits theme=get_dropdown<CR>', { noremap = true })
-vim.api.nvim_set_keymap('', '<A-c>', '<Cmd>Telescope git_commits theme=get_dropdown<CR>', { noremap = true })
-vim.api.nvim_set_keymap('i', '<A-s>', '<Cmd>Telescope git_stash theme=get_dropdown<CR>', { noremap = true })
-vim.api.nvim_set_keymap('', '<A-s>', '<Cmd>Telescope git_stash theme=get_dropdown<CR>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<A-t>', '<Cmd>Telescope asynctasks all theme=get_dropdown<CR>', { noremap = true })
+vim.api.nvim_set_keymap('', '<A-p>', '<Cmd>SaveSession<CR><Cmd>Telescope sessions theme=get_dropdown<CR>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<A-p>', '<Cmd>SaveSession<CR><Cmd>Telescope sessions theme=get_dropdown<CR>', { noremap = true })
 if vim.fn.has('win32') == 1 then
-  vim.api.nvim_set_keymap('', '<C-/>', '<Cmd>Telescope live_grep theme=get_dropdown<CR>', { noremap = true })
-  vim.api.nvim_set_keymap('i', '<C-/>', '<Esc><Cmd>Telescope live_grep theme=get_dropdown<CR>', { noremap = true })
+  vim.api.nvim_set_keymap('', '<C-/>', '<Cmd>Telescope live_grep<CR>', { noremap = true })
+  vim.api.nvim_set_keymap('i', '<C-/>', '<Esc><Cmd>Telescope live_grep<CR>', { noremap = true })
 else
-  vim.api.nvim_set_keymap('', '<C-_>', '<Cmd>Telescope live_grep theme=get_dropdown<CR>', { noremap = true })
-  vim.api.nvim_set_keymap('i', '<C-_>', '<Esc><Cmd>Telescope live_grep theme=get_dropdown<CR>', { noremap = true })
+  vim.api.nvim_set_keymap('', '<C-_>', '<Cmd>Telescope live_grep<CR>', { noremap = true })
+  vim.api.nvim_set_keymap('i', '<C-_>', '<Esc><Cmd>Telescope live_grep<CR>', { noremap = true })
 end
