44c96b609eec14aa30ba5f310f82262cbcac8391
Hennadii Chernyshchyk <genaloner@gmail.com>
2021-09-09 23:38:48 +0300

    Use Constant group to highlight DapStopped

--- plugin/dap.lua
+++ plugin/dap.lua
@@ -23,7 +23,7 @@ vim.g.dap_virtual_text = true
 
 vim.fn.sign_define('DapBreakpoint', { text = '', texthl = 'LspDiagnosticsDefaultError' })
 vim.fn.sign_define('DapLogPoint', { text = '', texthl = 'LspDiagnosticsDefaultInformation' })
-vim.fn.sign_define('DapStopped', { text = '' })
+vim.fn.sign_define('DapStopped', { text = '', texthl = 'Constant' })
 vim.fn.sign_define('DapBreakpointRejected', { text = '' })
 
 vim.api.nvim_set_keymap('n', '<F10>', '<Cmd>lua require("dap").step_over()<CR>', { noremap = true })
