3bb603cab0cba687a59ec92a08512e60c5d2d42f
Hennadii Chernyshchyk <genaloner@gmail.com>
2021-10-12 19:48:25 +0300

    Update plugins

--- plugin/neogit.lua
+++ plugin/neogit.lua
@@ -1,6 +1,7 @@
 require('neogit').setup({
   disable_commit_confirmation = true,
   disable_builtin_notifications = true,
+  disable_hint = true,
   signs = {
     section = { '', '' },
     item = { '', '' },
