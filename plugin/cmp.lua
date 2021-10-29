f0926adcc6ea6e1976d1bdf180067af23b7e658c
Hennadii Chernyshchyk <genaloner@gmail.com>
2021-10-29 22:33:37 +0300

    Add cmp-cmdline

--- plugin/cmp.lua
+++ plugin/cmp.lua
@@ -67,3 +67,11 @@ cmp.setup.cmdline('/', {
     { name = 'buffer' },
   },
 })
+
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({
+    { name = 'path' },
+  }, {
+    { name = 'cmdline' },
+  }),
+})
