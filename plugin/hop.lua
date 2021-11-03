392c5cbd12017949ce40653f56e14eb43de60c73
Hennadii Chernyshchyk <genaloner@gmail.com>
2021-11-04 00:28:24 +0200

    Update plugins

--- plugin/hop.lua
+++ plugin/hop.lua
@@ -1,3 +1,5 @@
+require('hop').setup()
+
 vim.api.nvim_set_keymap('', '<Leader>w', '<Cmd>HopWord<CR>', { noremap = true })
 vim.api.nvim_set_keymap('', '<Leader>/', '<Cmd>HopPattern<CR>', { noremap = true })
 vim.api.nvim_set_keymap('', '<Leader>d', '<Cmd>HopLine<CR>', { noremap = true })
