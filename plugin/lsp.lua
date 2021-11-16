72056f34126302a8db5c2b2cbf8a6105283e5559
Hennadii Chernyshchyk <genaloner@gmail.com>
2021-11-16 21:43:57 +0200

    Use rounded borders for hover and signature help

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -106,3 +106,11 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagn
   signs = true,
   update_in_insert = false,
 })
+
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
+  border = 'rounded',
+})
+
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+  border = 'rounded',
+})
