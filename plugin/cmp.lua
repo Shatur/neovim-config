aad33f9d7775cf301ec99000221fd76c7d435d90
Hennadii Chernyshchyk <genaloner@gmail.com>
2021-11-18 22:28:18 +0200

    Fix mappings conflict between nvim-cmp and luasnip

--- plugin/cmp.lua
+++ plugin/cmp.lua
@@ -38,8 +38,8 @@ cmp.setup({
     { name = 'cmp_git' },
   },
   mapping = {
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({ select = false }),
     ['<C-Space>'] = function()
--- plugin/luasnip.lua
+++ plugin/luasnip.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_set_keymap('i', '<C-d>', '<Plug>luasnip-expand-or-jump', {})
-vim.api.nvim_set_keymap('i', '<C-f>', '<Plug>luasnip-jump-prev', {})
-vim.api.nvim_set_keymap('s', '<C-d>', '<Plug>luasnip-expand-or-jump', { noremap = true })
-vim.api.nvim_set_keymap('s', '<C-f>', '<Plug>luasnip-jump-prev', { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-f>', '<Plug>luasnip-expand-or-jump', {})
+vim.api.nvim_set_keymap('i', '<C-b>', '<Plug>luasnip-jump-prev', {})
+vim.api.nvim_set_keymap('s', '<C-f>', '<Plug>luasnip-expand-or-jump', {})
+vim.api.nvim_set_keymap('s', '<C-b>', '<Plug>luasnip-jump-prev', {})
