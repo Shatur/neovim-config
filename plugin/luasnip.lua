e2b6bf675f1517096c724bb4cc01b36161f3a66b
Hennadii Chernyshchyk <genaloner@gmail.com>
2021-11-18 23:17:37 +0200

    Use require("neogen").jump_next() when available

--- plugin/luasnip.lua
+++ plugin/luasnip.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_set_keymap('i', '<C-f>', '<Plug>luasnip-expand-or-jump', {})
-vim.api.nvim_set_keymap('i', '<C-b>', '<Plug>luasnip-jump-prev', {})
-vim.api.nvim_set_keymap('s', '<C-f>', '<Plug>luasnip-expand-or-jump', {})
-vim.api.nvim_set_keymap('s', '<C-b>', '<Plug>luasnip-jump-prev', {})
+vim.api.nvim_set_keymap('i', '<C-f>', [[<Cmd>lua if require("neogen").jumpable() then require("neogen").jump_next() else require("luasnip").expand_or_jump() end <CR>]], {})
+vim.api.nvim_set_keymap('i', '<C-b>', '<Cmd>lua require("luasnip").jump(-1)<CR>', {})
+vim.api.nvim_set_keymap('s', '<C-f>', '<Cmd>lua require("luasnip").expand_or_jump()<CR>', {})
+vim.api.nvim_set_keymap('s', '<C-b>', '<Cmd>lua require("luasnip").jump(-1)<CR>', {})
