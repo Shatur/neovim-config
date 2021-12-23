c6e3d3b3eaa48d46ce5256ebc79c9b227d93d12a
Hennadii Chernyshchyk <genaloner@gmail.com>
2021-12-23 18:13:45 +0200

    Remove nvim-ts-rainbow

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -6,11 +6,6 @@ require('nvim-treesitter.configs').setup({
   playground = {
     enable = true,
   },
-  rainbow = {
-    enable = true,
-    extended_mode = true,
-    max_file_lines = 1000,
-  },
   textobjects = {
     select = {
       enable = true,
