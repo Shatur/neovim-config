1e72dd17b436615c8d1a00e5b127b1c4f1f89eeb
Hennadii Chernyshchyk <genaloner@gmail.com>
2022-01-01 08:01:33 +0200

    Remove neogit stickybuf settings

--- plugin/stickybuf.lua
+++ plugin/stickybuf.lua
@@ -1,10 +1,8 @@
 require('stickybuf').setup({
   filetype = {
     startuptime = 'filetype',
-    NeogitCommitView = 'filetype',
   },
   bufname = {
-    ['.*NEOGIT_COMMIT_EDITMSG'] = 'bufnr',
     ['DAP *.'] = 'bufnr',
   },
 })
