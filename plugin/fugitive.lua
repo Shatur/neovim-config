6f55d36df2f8da510024baebbcd8e079acfcf073
Hennadii Chernyshchyk <genaloner@gmail.com>
2022-04-24 22:32:29 +0300

    Add keymaps for git commands

--- plugin/fugitive.lua
+++ plugin/fugitive.lua
@@ -1,1 +1,31 @@
+local function run_command(command)
+  local notification
+  local title = 'Running ' .. table.concat(command, ' ')
+
+  local output = ''
+  local on_data = function(_, data)
+    output = output .. table.concat(data, '\n')
+    notification = vim.notify(output, vim.log.levels.INFO, { title = title, replace = notification })
+  end
+
+  vim.fn.jobstart(command, {
+    on_stdout = on_data,
+    on_stderr = on_data,
+    on_exit = function(_, code)
+      if code ~= 0 then
+        vim.notify(output .. '\nExited with code: ' .. code, vim.log.levels.ERROR, { title = title, replace = notification })
+      end
+    end,
+  })
+end
+
 vim.keymap.set({ '', 't', 'i' }, '<A-g>', '<Cmd>vert Git<CR>', { noremap = true })
+vim.keymap.set('', '<Leader>gP', function()
+  run_command({ 'git', 'pull' })
+end, { noremap = true })
+vim.keymap.set('', '<Leader>gp', function()
+  run_command({ 'git', 'push' })
+end, { noremap = true })
+vim.keymap.set('', '<Leader>gfp', function()
+  run_command({ 'git', 'push', '--force' })
+end, { noremap = true })
