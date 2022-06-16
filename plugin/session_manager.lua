2dd260ca67097778ce0e89ac55580fec21eb24d2
Hennadii Chernyshchyk <genaloner@gmail.com>
2022-06-16 19:59:23 +0300

    Add toggleterm to autosave_ignore_filetypes

--- plugin/session_manager.lua
+++ plugin/session_manager.lua
@@ -1,1 +1,10 @@
-vim.keymap.set({ '', 't', 'i' }, '<A-p>', require('session_manager').load_session, { noremap = true })
+local session_manager = require('session_manager')
+
+session_manager.setup({
+  autosave_ignore_filetypes = {
+    'gitcommit',
+    'toggleterm',
+  },
+})
+
+vim.keymap.set({ '', 't', 'i' }, '<A-p>', session_manager.load_session, { noremap = true })
