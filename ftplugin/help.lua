410b55faa63fa92ae18779a6a1742a9defddebde
Hennadii Chernyshchyk <genaloner@gmail.com>
2022-06-19 00:46:58 +0300

    Remove old workaround for FZF from help

--- ftplugin/help.lua
+++ ftplugin/help.lua
@@ -1,8 +1,3 @@
-if vim.api.nvim_win_get_height(0) == 1 then
-  -- If the height is 1, then we show the window inside FZF, do not set any options
-  return
-end
-
 vim.api.nvim_command('wincmd L')
 vim.api.nvim_command('vertical resize 82')
 vim.api.nvim_command('setlocal winfixwidth')
