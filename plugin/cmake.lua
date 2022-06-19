72b1ada619a61db1baff554eb76a21800a63a427
Hennadii Chernyshchyk <genaloner@gmail.com>
2022-06-19 16:55:00 +0300

    Update plugins

--- plugin/cmake.lua
+++ plugin/cmake.lua
@@ -18,38 +18,15 @@ else
 end
 cmake.setup(config)
 
-local function save_all()
-  for _, buffer in ipairs(vim.api.nvim_list_bufs()) do
-    if #vim.api.nvim_buf_get_name(buffer) ~= 0 and vim.api.nvim_buf_get_option(buffer, 'modified') then
-      vim.api.nvim_command('silent write')
-    end
-  end
-end
-
 vim.keymap.set({ '', 'i' }, '<C-BS>', cmake.cancel, { noremap = true })
-vim.keymap.set({ '', 'i' }, '<F5>', function()
-  save_all()
-  cmake.build_and_debug()
-end, { noremap = true })
+vim.keymap.set({ '', 'i' }, '<F5>', cmake.build_and_debug, { noremap = true })
 vim.keymap.set({ '', 'i' }, '<S-F5>', cmake.set_target_args, { noremap = true })
 vim.keymap.set({ '', 'i' }, '<A-F5>', cmake.debug, { noremap = true })
-vim.keymap.set({ '', 'i' }, '<F6>', function()
-  save_all()
-  cmake.build_and_run()
-end, { noremap = true })
+vim.keymap.set({ '', 'i' }, '<F6>', cmake.build_and_run, { noremap = true })
 vim.keymap.set({ '', 'i' }, '<A-F6>', cmake.run, { noremap = true })
-vim.keymap.set({ '', 'i' }, '<F7>', function()
-  save_all()
-  cmake.build()
-end, { noremap = true })
+vim.keymap.set({ '', 'i' }, '<F7>', cmake.build, { noremap = true })
 vim.keymap.set({ '', 'i' }, '<S-F7>', cmake.select_target, { noremap = true })
-vim.keymap.set({ '', 'i' }, '<A-F7>', function()
-  save_all()
-  cmake.build_all()
-end, { noremap = true })
-vim.keymap.set({ '', 'i' }, '<F8>', function()
-  save_all()
-  cmake.configure()
-end, { noremap = true })
+vim.keymap.set({ '', 'i' }, '<A-F7>', cmake.build_all, { noremap = true })
+vim.keymap.set({ '', 'i' }, '<F8>', cmake.configure, { noremap = true })
 vim.keymap.set({ '', 'i' }, '<S-F8>', cmake.select_build_type, { noremap = true })
 vim.keymap.set({ '', 'i' }, '<A-F8>', cmake.clear_cache, { noremap = true })
