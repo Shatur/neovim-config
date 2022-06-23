c5d5f4fceab8c82dca88a6740f639b450305a7f9
Hennadii Chernyshchyk <genaloner@gmail.com>
2022-06-23 21:26:45 +0300

    Disable autopairs event in Rust

--- plugin/cmp.lua
+++ plugin/cmp.lua
@@ -1,7 +1,6 @@
 local luasnip = require('luasnip')
 local cmp = require('cmp')
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local handlers = require('nvim-autopairs.completion.handlers')
 
 local completion_types = {
   Text = { kind = '', menu = 'Text' },
@@ -90,14 +89,8 @@ cmp.setup.cmdline(':', {
 cmp.event:on(
   'confirm_done',
   cmp_autopairs.on_confirm_done({
-    ['lua'] = {
-      ['('] = {
-        kind = {
-          cmp.lsp.CompletionItemKind.Function,
-          cmp.lsp.CompletionItemKind.Method,
-        },
-        handler = handlers['*'],
-      },
+    filetypes = {
+      rust = false,
     },
   })
 )
