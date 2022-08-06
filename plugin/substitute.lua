6eaf68fd1f5f43b639bda40487875c05cd64ed38
Hennadii Chernyshchyk <genaloner@gmail.com>
2022-08-07 01:01:10 +0300

    Change keymaps with s

--- plugin/substitute.lua
+++ plugin/substitute.lua
@@ -1,6 +1,6 @@
 local substitute = require('substitute')
 
-vim.keymap.set('n', '<Leader>r', substitute.operator, { noremap = true })
-vim.keymap.set('n', '<Leader>rr', substitute.line, { noremap = true })
-vim.keymap.set('n', '<Leader>R', substitute.eol, { noremap = true })
+vim.keymap.set('n', 's', substitute.operator, { noremap = true })
+vim.keymap.set('n', 'ss', substitute.line, { noremap = true })
+vim.keymap.set('n', 'S', substitute.eol, { noremap = true })
 vim.keymap.set('x', 'p', substitute.visual, { noremap = true })
