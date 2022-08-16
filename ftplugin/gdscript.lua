8bf5bed0d9afe9a6a0b0e0846fb29154d7e3c67c
Hennadii Chernyshchyk <genaloner@gmail.com>
2022-08-16 19:41:45 +0300

    Remove gdscript ftplugin

--- ftplugin/gdscript.lua
+++
@@ -1,5 +0,0 @@
-vim.api.nvim_command('setlocal nowrap')
-vim.bo.cinkeys = vim.bo.cinkeys:gsub('0#', '')
-vim.bo.indentkeys = vim.bo.indentkeys:gsub('0#', '')
-vim.bo.suffixesadd = '.gd'
-vim.bo.commentstring = '#\\ %s'
