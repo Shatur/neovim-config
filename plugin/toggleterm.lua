10ac4d60779d55b25ced5a0f520d8de7de293b5c
Hennadii Chernyshchyk <genaloner@gmail.com>
2022-11-13 01:48:30 +0200

    Use <C-`> to toggle terminal

--- plugin/toggleterm.lua
+++ plugin/toggleterm.lua
@@ -1,6 +1,6 @@
 require('toggleterm').setup({
   shell = vim.fn.has('win32') == 1 and 'pwsh -NoLogo' or '/usr/bin/fish',
-  open_mapping = '<F4>',
+  open_mapping = '<C-`>',
   shade_terminals = false,
   on_open = function() vim.wo.spell = false end,
 })
