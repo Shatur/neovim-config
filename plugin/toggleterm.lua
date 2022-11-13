09cec4b9b5aed983d0abcca3d5c3473d02ed54bc
Hennadii Chernyshchyk <genaloner@gmail.com>
2022-11-14 00:44:43 +0200

    Switch back to <F4> to toggle terminal

--- plugin/toggleterm.lua
+++ plugin/toggleterm.lua
@@ -1,6 +1,6 @@
 require('toggleterm').setup({
   shell = vim.fn.has('win32') == 1 and 'pwsh -NoLogo' or '/usr/bin/fish',
-  open_mapping = '<C-`>',
+  open_mapping = '<F4>',
   shade_terminals = false,
   on_open = function() vim.wo.spell = false end,
 })
