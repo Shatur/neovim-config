908fe35ef3c260381e121328bcc2604fd5cb7d64
Shatur <genaloner@gmail.com>
2023-01-15 16:42:44 +0200

    Use bdelete to redraw bufferline

--- plugin/buffers.lua
+++ plugin/buffers.lua
@@ -63,7 +63,7 @@ local function close_other_buffers()
   local current_buffer = vim.api.nvim_get_current_buf()
   for _, buffer in ipairs(vim.api.nvim_list_bufs()) do
     if buffer ~= current_buffer and not vim.api.nvim_buf_get_option(buffer, 'modified') and vim.api.nvim_buf_get_option(buffer, 'buflisted') then
-      vim.api.nvim_buf_delete(buffer, {})
+      vim.cmd.bdelete({ buffer })
     end
   end
 end
