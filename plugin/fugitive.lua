0807e626e2b7b89278f16fdb223526c27758128a
Hennadii Chernyshchyk <genaloner@gmail.com>
2023-02-10 02:10:24 +0200

    Fix force push keymap

--- plugin/fugitive.lua
+++ plugin/fugitive.lua
@@ -3,4 +3,4 @@ vim.g.fugitive_legacy_commands = false
 vim.keymap.set({ '', 't' }, '\\', '<Cmd>vert Git<CR>))', { noremap = true, desc = 'Open Fugitive' })
 vim.keymap.set('', '<Leader>gP', function() vim.cmd.Git('pull') end, { noremap = true, desc = 'Git pull' })
 vim.keymap.set('', '<Leader>gp', function() vim.cmd.Git('push') end, { noremap = true, desc = 'Git push' })
-vim.keymap.set('', '<Leader>gfp', function() vim.cmd.Git('push', '--force') end, { noremap = true, desc = 'Git push (force)' })
+vim.keymap.set('', '<Leader>gfp', function() vim.cmd.Git('push --force') end, { noremap = true, desc = 'Git push (force)' })
