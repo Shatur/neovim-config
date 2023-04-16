32f67963fc6b5dfaf4585fa1a3b52094b82653dc
Hennadii Chernyshchyk <genaloner@gmail.com>
2023-04-16 17:41:34 +0300

    Do not trigger <Leader>gg in terminal

--- plugin/fugitive.lua
+++ plugin/fugitive.lua
@@ -1,6 +1,6 @@
 vim.g.fugitive_legacy_commands = false
 
-vim.keymap.set({ '', 't' }, '<Leader>gg', '<Cmd>vert Git<CR>))', { noremap = true, desc = 'Open Fugitive' })
+vim.keymap.set('', '<Leader>gg', '<Cmd>vert Git<CR>))', { noremap = true, desc = 'Open Fugitive' })
 vim.keymap.set('', '<Leader>gP', function() vim.cmd.Git('pull') end, { noremap = true, desc = 'Git pull' })
 vim.keymap.set('', '<Leader>gp', function() vim.cmd.Git('push') end, { noremap = true, desc = 'Git push' })
 vim.keymap.set('', '<Leader>gfp', function() vim.cmd.Git('push --force') end, { noremap = true, desc = 'Git push (force)' })
