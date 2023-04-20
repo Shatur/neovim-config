e63a04d5f5ff493e903e4030a95200cd71e8b018
Hennadii Chernyshchyk <genaloner@gmail.com>
2023-04-20 23:44:24 +0300

    Remove buffer search completion
    Too slow.

--- plugin/cmp.lua
+++ plugin/cmp.lua
@@ -69,13 +69,6 @@ cmp.setup({
   },
 })
 
-cmp.setup.cmdline('/', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' },
-  },
-})
-
 cmp.setup.cmdline(':', {
   mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
