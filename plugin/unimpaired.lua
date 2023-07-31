95d450f85ccfcd0302c49e15be1195d1a4762724
Hennadii Chernyshchyk <genaloner@gmail.com>
2023-07-31 15:59:12 +0300

    Replace vim-unimpaired with unimpaired.nvim

---
+++ plugin/unimpaired.lua
@@ -0,0 +1,11 @@
+require('unimpaired').setup({
+  bfirst = false,
+  blast = false,
+  bnext = false,
+  bprevious = false,
+  next_file = false,
+  previous_file = false,
+  tfirst = false,
+  tlast = false,
+  tnext = false,
+})
