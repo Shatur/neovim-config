0825d263ed3ab7e45fcd242313d1dec354453f76
Hennadii Chernyshchyk <genaloner@gmail.com>
2023-09-18 00:19:53 +0300

    Fix unimpaired configuration

--- plugin/unimpaired.lua
+++ plugin/unimpaired.lua
@@ -1,11 +1,13 @@
 require('unimpaired').setup({
-  bfirst = false,
-  blast = false,
-  bnext = false,
-  bprevious = false,
-  next_file = false,
-  previous_file = false,
-  tfirst = false,
-  tlast = false,
-  tnext = false,
+  keymaps = {
+    bfirst = false,
+    blast = false,
+    bnext = false,
+    bprevious = false,
+    next_file = false,
+    previous_file = false,
+    tfirst = false,
+    tlast = false,
+    tnext = false,
+  },
 })
