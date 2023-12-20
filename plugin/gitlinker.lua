c8ac1904491c80e094d608513a3415992be65b50
Hennadii Chernyshchyk <genaloner@gmail.com>
2023-12-21 01:40:44 +0200

    Fix GitLinker config

--- plugin/gitlinker.lua
+++ plugin/gitlinker.lua
@@ -1,16 +1,1 @@
-local gitlinker = require('gitlinker')
-gitlinker.setup()
-
-vim.api.nvim_create_user_command(
-  'GitLink',
-  function()
-    gitlinker.link({
-      action = require('gitlinker.actions').system,
-      lstart = vim.api.nvim_buf_get_mark(0, '<')[1],
-      lend = vim.api.nvim_buf_get_mark(0, '>')[1],
-    })
-  end,
-  {
-    range = true,
-  }
-)
+require('gitlinker').setup()
