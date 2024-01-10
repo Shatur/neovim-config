8887bf2de26988139caef447a97cf13a16dadc73
Hennadii Chernyshchyk <genaloner@gmail.com>
2024-01-10 17:04:07 +0200

    Remove lsp_lines.nvim

--- plugin/diagnostic.lua
+++ plugin/diagnostic.lua
@@ -1,16 +1,10 @@
-require('lsp_lines').setup()
-
 vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
 vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
 vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
 vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
 
-local virtual_text = { prefix = '' }
-local virtual_lines = false
-
 vim.diagnostic.config({
-  virtual_text = virtual_text,
-  virtual_lines = virtual_lines,
+  virtual_text = { prefix = '' },
   float = {
     border = 'rounded',
   },
@@ -28,10 +22,4 @@ local function toggle_disanostic()
   end
 end
 
-local function toggle_virtual_lines()
-  virtual_lines = not virtual_lines
-  vim.diagnostic.config({ virtual_lines = virtual_lines, virtual_text = not virtual_lines and virtual_text or false })
-end
-
 vim.keymap.set('n', 'yoD', toggle_disanostic, { noremap = true, desc = 'Toggle diagnostic' })
-vim.keymap.set('n', 'yoL', toggle_virtual_lines, { noremap = true, desc = 'Toggle virtual lines' })
