54dd3235f12f7079b99c4d9b5912ff8ceb9841a9
Hennadii Chernyshchyk <genaloner@gmail.com>
2024-05-23 17:56:02 +0300

    Remove neogen properly

--- plugin/luasnip.lua
+++ plugin/luasnip.lua
@@ -1,15 +1,6 @@
-local neogen = require('neogen')
 local luasnip = require('luasnip')
 
-local function expand_or_jump()
-  if neogen.jumpable() then
-    neogen.jump_next()
-  else
-    luasnip.expand_or_jump()
-  end
-end
-
 for _, mode in ipairs({ 's', 'i' }) do
-  vim.keymap.set(mode, '<C-f>', expand_or_jump, { desc = 'Jump to next snippet' })
+  vim.keymap.set(mode, '<C-f>', luasnip.expand_or_jump, { desc = 'Jump to next snippet' })
   vim.keymap.set(mode, '<C-b>', function() luasnip.jump(-1) end, { desc = 'Jump to previous snippet' })
 end
--- plugin/neogen.lua
+++
@@ -1,7 +0,0 @@
-local neogen = require('neogen')
-
-neogen.setup({
-  enabled = true,
-})
-
-vim.keymap.set('n', '<Leader>n', neogen.generate, { noremap = true, desc = 'Generate documentation' })
