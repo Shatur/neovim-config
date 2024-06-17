2a56243fd13dd14c92e84fd86c87e1f1cfdb6c1b
Hennadii Chernyshchyk <genaloner@gmail.com>
2024-06-17 11:26:22 +0300

    Rework shortcuts

--- plugin/vscode.lua
+++ plugin/vscode.lua
@@ -4,11 +4,20 @@ end
 
 local vscode = require('vscode')
 
-vim.keymap.set('n', ']b', function() vscode.call('workbench.action.nextEditorInGroup') end, { noremap = true })
-vim.keymap.set('n', '[b', function() vscode.call('workbench.action.previousEditorInGroup') end, { noremap = true })
-vim.keymap.set('n', '[B', function() vscode.call('workbench.action.firstEditorInGroup') end, { noremap = true })
-vim.keymap.set('n', ']B', function() vscode.call('workbench.action.lastEditorInGroup') end, { noremap = true })
-vim.keymap.set('n', ']t', function() vscode.call('workbench.action.nextEditorGroup') end, { noremap = true })
-vim.keymap.set('n', '[t', function() vscode.call('workbench.action.previousEditorGroup') end, { noremap = true })
-vim.keymap.set('n', ']d', function() vscode.call('editor.action.marker.next') end, { noremap = true })
-vim.keymap.set('n', '[d', function() vscode.call('editor.action.marker.prev') end, { noremap = true })
+vim.keymap.set('', ']b', function() vscode.call('workbench.action.nextEditorInGroup') end, { noremap = true })
+vim.keymap.set('', '[b', function() vscode.call('workbench.action.previousEditorInGroup') end, { noremap = true })
+vim.keymap.set('', '[B', function() vscode.call('workbench.action.firstEditorInGroup') end, { noremap = true })
+vim.keymap.set('', ']B', function() vscode.call('workbench.action.lastEditorInGroup') end, { noremap = true })
+
+vim.keymap.set('', ']t', function() vscode.call('workbench.action.nextEditorGroup') end, { noremap = true })
+vim.keymap.set('', '[t', function() vscode.call('workbench.action.previousEditorGroup') end, { noremap = true })
+
+vim.keymap.set('', ']d', function() vscode.call('editor.action.marker.next') end, { noremap = true })
+vim.keymap.set('', '[d', function() vscode.call('editor.action.marker.prev') end, { noremap = true })
+
+vim.keymap.set('', ']c', function() vscode.call('workbench.action.editor.nextChange') end, { noremap = true })
+vim.keymap.set('', '[c', function() vscode.call('workbench.action.editor.previousChange') end, { noremap = true })
+
+vim.keymap.set('', 'ghr', function() vscode.call('git.revertSelectedRanges') end, { noremap = true })
+vim.keymap.set('', 'ghs', function() vscode.call('git.stageSelectedRanges') end, { noremap = true })
+vim.keymap.set('', 'ghu', function() vscode.call('git.unstageSelectedRanges') end, { noremap = true })
